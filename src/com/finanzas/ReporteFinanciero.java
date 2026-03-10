39f68c4858df9bf275fb9d7b86f6b21ebce3d4a4
ale-820 <medinaalenni435@gmail.com>
2026-03-09 19:19:46 -0500

    Sistema de reportes financieros

---
+++ src/com/finanzas/ReporteFinanciero.java
@@ -0,0 +1,12 @@
+package com.finanzas;
+
+import java.util.ArrayList;
+
+public class ReporteFinanciero {
+
+    public static double calcularBalance(double ingresos, double gastos){
+
+        return ingresos - gastos;
+
+    }
+}
